fd00aadf92587c6100c88bde0cb7ef65fffc3d4e
mahbubur-r-aaman <mahbubur.r.aaman@gmail.com>
2016-11-17 18:08:47 +0600

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 
     <section class="main-content">
     <br/> <br/>
-    <a href="/iOS-Universal-Links/oneapp.html">OneApp123</a>
       
 <?php
   #remove the directory path we don't want
@@ -16,13 +15,13 @@
  
   #split the path by '/'
   $params     = split("/", $request);
-  print "<br/> <p>" . "parameter: " . $params[0] . "</p>";
+  print "<br/> <br/> <p>" . "parameter: " . $params[0] . "</p>";
       
-  print "\r\n";
+  
   
  // print "Request Operating System: " . $_SERVER['HTTP_USER_AGENT'];
       
-    print "\r\n";
+
       
 $iPod    = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
 $iPhone  = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
@@ -32,13 +31,13 @@ $Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android");
 //do something with this information
 if( $iPod || $iPhone || $iPad){
    // print "Request Operating System is : iOS";
-    print "<br/> <p>" . "parameter: " . "Request Operating System is : iOS" . "</p>";
+    print "<br/> <br/> <p>". "Request Operating System is : iOS" . "</p>";
 } else if($Android){
     //print "Request Operating System is : Android";
-    print "<br/> <p>" . "parameter: " . "Request Operating System is : Android" . "</p>";
+    print "<br/> <br/> <p>". "Request Operating System is : Android" . "</p>";
 }else if($webOS){
     //print "Request Operating System is : webOS";
-    print "<br/> <p>" . "parameter: " . "Request Operating System is : Android" . "</p>";
+    print "<br> <br/> <p>" . "Request Operating System is : Android" . "</p>";
 }
   
  // print "parma 1: " . $params[1];
