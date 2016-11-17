bba111440982f24535dca19784d13654d97f2e40
mahbubur-r-aaman <mahbubur.r.aaman@gmail.com>
2016-11-17 15:41:04 +0600

    Update index.php

--- oneapp/index.php
+++ oneapp/index.php
@@ -1,8 +1,8 @@
 <?php
   #remove the directory path we don't want
-  $request  = str_replace("/envato/pretty/php/", "", $_SERVER['REQUEST_URI']);
+  // $request  = str_replace("/envato/pretty/php/", "", $_SERVER['REQUEST_URI']);
  
   #split the path by '/'
-  $params     = split("/", $request);
+  $params     = split("/", $_SERVER['REQUEST_URI']);
   print $params; 
 ?>
