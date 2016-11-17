87763f390c0440814e6c234a54336456368f4017
mahbubur-r-aaman <mahbubur.r.aaman@gmail.com>
2016-11-17 16:06:51 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,14 @@
 <!DOCTYPE html>
 <html lang="en-us">
   <head>
-    <meta charset="UTF-8">
-    <title>iOS Universal Links Demo</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="stylesheets/normalize.css" media="screen">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" media="screen">
-    <link rel="stylesheet" type="text/css" href="stylesheets/github-light.css" media="screen">
   </head>
   <body>
     <section class="page-header">
-      <h2 class="project-name">iOS Universal Links PoC</h2>
-      <h5 class="project-tagline">This webpage for iOS Universal Link PoC</h5>
- 
     </section>
 
     <section class="main-content">
- <!--     <a href="/iOS-Universal-Links/home.html">Home</a>
-      </br>
--->
-      <br/> <br/>
-      <a href="/iOS-Universal-Links/oneapp.html">OneApp123</a>
+    <br/> <br/>
+    <a href="/iOS-Universal-Links/oneapp.html">OneApp123</a>
       
 <?php
   #remove the directory path we don't want
@@ -29,6 +16,8 @@
  
   #split the path by '/'
   $params     = split("/", $request);
+  print $params[0];
+  print $params[1];
 ?>
       <footer class="site-footer">
       </footer>
