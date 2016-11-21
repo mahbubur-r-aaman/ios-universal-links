6ad9b800d8d752faad071518e81cb1f26b689e82
mahbubur-r-aaman <mahbubur.r.aaman@gmail.com>
2016-11-21 11:12:33 +0600

    Create install.php

---
+++ install.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+  </head>
+  <body>
+    <section class="page-header">
+    </section>
+
+    <section class="main-content">
+    <br/> <br/>
+      
+<?php
+      
+$iosRandVal = "pqrs4567"; 
+print "<br/> <br/> <p>" . "Parameter for iOS User: " . $iosRandVal . "</p>";     
+print ' <br/> <br/> <form> Just Copy the text, you will be directed to OneApp: <input type="text" name="iOSRandVal" value = '. $iosRandVal . ' </input><br></form> '; 
+print "<br/> <br/> <p>" . "<a href='https://itunes.apple.com/us/app/smart-home/id885787515?mt=8'> Open this app in AppStore </a> ". "</p>";  
+?>
+      
+      
+      <footer class="site-footer">
+      </footer>
+
+    </section>
+
+  
+  </body>
+</html>
